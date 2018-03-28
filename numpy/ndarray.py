1de48e5f23d8bc12018b8f30f4338d2915ad9989
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-03-28 15:19:45 +0000

    add: np.random.randの追加

--- numpy/ndarray.py
+++ numpy/ndarray.py
@@ -2,6 +2,12 @@ import numpy as np
 
 arr = np.array([1,2,3])
 print(arr)
+print(arr.shape)
 
 matrix = np.array([[1,2,3],[4,5,6]])
 print(matrix)
+print(matrix.shape)
+
+
+arr_random = np.random.rand(2, 4)
+print(arr_random)
